8d8aa72330cb7824bef0e52f93e715a83e8719ad
George Gonzalez <geg321@nyu.edu>
2023-03-31 14:12:01 -0400

    working on collectables

--- Assets/Scripts/DestroyByPlayer.cs
+++ Assets/Scripts/DestroyByPlayer.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDamager : MonoBehaviour
+public class DestroyByPlayer : MonoBehaviour
 {
 
-    public static int damageVal = 1;
     // Start is called before the first frame update
     void Start()
     {
