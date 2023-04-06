80ea870250206d4f2d59b2b29c3294895cdc5cd8
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-04-06 02:14:39 -0400

    ADD UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,13 @@ public class GameManager : MonoBehaviour
 {
     int score = 0;
     int health = 100;
-    GameObject _player;
+    Player _player;
+    TMP_Text scoreUI;
+    TMP_Text healthUI;
+    TMP_Text gunAmmoUI;
+    TMP_Text rockAmmoUI;
+    TMP_Text dynaAmmoUI;
+
 
     private void Awake()
     {
@@ -25,35 +31,69 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        scoreUI = GameObject.FindGameObjectWithTag("ScoreUI").GetComponent<TextMeshProUGUI>();
+        healthUI = GameObject.FindGameObjectWithTag("HealthUI").GetComponent<TextMeshProUGUI>();
+        gunAmmoUI = GameObject.FindGameObjectWithTag("GunUI").GetComponent<TextMeshProUGUI>();
+        rockAmmoUI = GameObject.FindGameObjectWithTag("RockUI").GetComponent<TextMeshProUGUI>();
+        dynaAmmoUI = GameObject.FindGameObjectWithTag("DynaUI").GetComponent<TextMeshProUGUI>();
+        scoreUI.text = "SCORE: " + score;
+        healthUI.text = "HEALTH: " + health;
         SceneManager.activeSceneChanged += SceneChange;
     }
 
     private void SceneChange(Scene current, Scene next) {
-        _player = GameObject.FindGameObjectWithTag("Player");
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        scoreUI = GameObject.FindGameObjectWithTag("ScoreUI").GetComponent<TextMeshProUGUI>();
+        healthUI = GameObject.FindGameObjectWithTag("HealthUI").GetComponent<TextMeshProUGUI>();
+        gunAmmoUI = GameObject.FindGameObjectWithTag("GunUI").GetComponent<TextMeshProUGUI>();
+        rockAmmoUI = GameObject.FindGameObjectWithTag("RockUI").GetComponent<TextMeshProUGUI>();
+        dynaAmmoUI = GameObject.FindGameObjectWithTag("DynaUI").GetComponent<TextMeshProUGUI>();
+        scoreUI.text = "SCORE: " + score;
+        healthUI.text = "HEALTH: " + health;
     }
 
     public void AddScore(int value) {
         score += value;
+        scoreUI.text = "SCORE: " + score;
     }
 
     public void ResetScore(int value) {
         score = 0;
+        scoreUI.text = "SCORE: " + score;
     }
 
     public void TakeDamage(int value) {
         health -= value; 
         health = Mathf.Clamp(health, 0, 100);
 
-        StartCoroutine(_player.GetComponent<Player>().DamageTaken());
+        StartCoroutine(_player.DamageTaken());
 
         if(health == 0) {
             SceneManager.LoadScene("Fail");
         }
+
+        healthUI.text = "HEALTH: " + health;
     }
 
     void Update()
     {
+        if(gunAmmoUI != null && _player.weaponType == "pistol") {
+            gunAmmoUI.text = "" + _player.bulletCount;
+        } else if(gunAmmoUI != null) {
+            gunAmmoUI.text = "---";
+        }
+
+
+        if(rockAmmoUI != null) {
+            rockAmmoUI.text = "" + _player.rockCount;
+        }
+
+        if(dynaAmmoUI != null) {
+            dynaAmmoUI.text = "" + _player.dynamiteCount;
+        }
+
+
 #if !UNITY_WEBGL
         if (Input.GetKeyDown(KeyCode.Escape))
         {
