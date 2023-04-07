0559a8d876b2be1a09fe7e00a933886dc1c83101
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-04-07 01:13:46 -0400

    Adjustments

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    private void Awake()
+    {
+        if (FindObjectsOfType<GameManager>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _audioSource = GetComponent<AudioSource>();
+            SceneManager.activeSceneChanged += SceneChange;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void SceneChange(Scene current, Scene next) {
+        if(next.name == "Fail") {
+            Destroy(gameObject);
+        }
+    }
+}
